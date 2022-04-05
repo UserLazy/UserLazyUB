2a1c6c217c0478085d6393efdd09dc685287e355
UserLazy <78595134+UserLazy@users.noreply.github.com>
2022-04-05 08:42:31 +0700

    string

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -273,10 +273,7 @@ with bot:
 async def update_restart_msg(chat_id, msg_id):
     DEFAULTUSER = ALIVE_NAME or "Set `ALIVE_NAME` ConfigVar!"
     message = (
-        f"**WeebProject is back up and running!**\n\n"
-        f"**Telethon :** __{version.__version__}__\n"
-        f"**Python :** __{python_version()}__\n"
-        f"**User :** __{DEFAULTUSER}__"
+        f"**__Update has been successfully__**"
     )
     await bot.edit_message(chat_id, msg_id, message)
     return True
