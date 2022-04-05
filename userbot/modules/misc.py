ee856c93689cb3d6d25f1cbaa7e525e0f024f934
UserLazy <78595134+UserLazy@users.noreply.github.com>
2022-04-05 08:29:55 +0700

    optimized

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -107,7 +107,7 @@ async def repeat(rep):
 @register(outgoing=True, pattern=r"^\.repo$")
 async def repo_is_here(wannasee):
     """For .repo command, just returns the repo URL."""
-    await wannasee.edit("Here is my [Repo](https://github.com/BianSepang/WeebProject)")
+    await wannasee.edit("Here is my [Repo](https://github.com/UserLazy/UserLazyUB)")
 
 
 @register(outgoing=True, pattern=r"^\.raw$")
